6fb5cbb456dac1f20f6e71a00fde180099a6b853
Raza Bhatti <R.Bhatti@ceswest.ca>
2021-04-30 12:15:31 -0600

    Updated code to run on windows error free as well

--- ces.py
+++ ces.py
@@ -1,4 +1,4 @@
-print 'This is a testing for CI-CD pinelines for CES'
-print "Adding a new line on previous program"
-print "Adding another new line on previous program"
-print "Pi restart tewt --- another new line on previous program"
+print ("This is a testing for CI-CD pinelines for CES")
+print ('Adding a new line on previous program')
+print ("Adding another new line on previous program")
+print ('Pi restart test --- another new line on previous program')
